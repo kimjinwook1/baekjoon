d4e8ed8b0a117f69b6cc5df370bd347871f87727
kimjinwook1 <rlawlsdnr628@naver.com>
2022-05-27 20:50:22 +0900

    백준 15596 정수 N개의 합

---
+++ src/baekjoon/Baek15596.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+public class Baek15596 {
+
+	public class Test {
+		long sum(int[] a) {
+			long ans = 0;
+			for (int i : a) {
+				ans += i;
+			}
+			return ans;
+		}
+	}
+}

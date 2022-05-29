74f31e29969b5e2d7b01b7eb3ca397597b5d7d25
kimjinwook1 <rlawlsdnr628@naver.com>
2022-05-29 16:23:30 +0900

    백준 11654 아스키 코드

---
+++ src/baekjoon/Baek11654.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baek11654 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println((int) scanner.nextLine().charAt(0));
+
+	}
+}

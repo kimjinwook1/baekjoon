3e0abd2ca3f280d03ecb4316639954a3dfbedcbe
kimjinwook1 <rlawlsdnr628@naver.com>
2022-06-19 17:33:35 +0900

    백준 10757 큰 수 A+B

---
+++ src/baekjoon/Baek10757.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Baek10757 {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		BigInteger first = sc.nextBigInteger();
+		BigInteger second = sc.nextBigInteger();
+		System.out.println(first.add(second));
+	}
+}
